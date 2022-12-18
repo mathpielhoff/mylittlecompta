048e51f5ebc77220388f175033e7479a11fd5f0a
peyah <peyah42078@wirese.com>
2022-12-18 17:15:39 +0100

    flask-security and flask-restful

--- app/__init__.py
+++ app/__init__.py
@@ -1,42 +1,33 @@
 import logging
-from flask import Flask
-from app.index import MyIndexView
-from flask_appbuilder.security.mongoengine.manager import SecurityManager
-from flask_appbuilder.menu import Menu
-from flask_appbuilder import AppBuilder
+from flask import Flask, render_template_string
 from flask_mongoengine import MongoEngine
-from flask_bootstrap import Bootstrap5
+from flask_security import Security, MongoEngineUserDatastore, auth_required, hash_password
+from flask_restful import Resource, Api
+from flask_wtf.csrf import CSRFProtect
+from app.endpoint import ContactAPI
+
+# Custom imports
+from app.models import User, Role
 
 """
  Logging configuration
 """
-
 logging.basicConfig(format='%(asctime)s:%(levelname)s:%(name)s:%(message)s')
 logging.getLogger().setLevel(logging.DEBUG)
 
 app = Flask(__name__)
 app.config.from_object('config')
+
+# Adding RestFull
+api = Api(app)
+api.add_resource(ContactAPI, '/api/contact')
+# Adding Mongo
 db = MongoEngine(app)
-bootstrap = Bootstrap5(app)
-appbuilder = AppBuilder(app, 
-    security_manager_class=SecurityManager, 
-    indexview=MyIndexView,
-    base_template='base.html',
-    menu=Menu(reverse=False)
-    )
 
-"""
-from sqlalchemy.engine import Engine
-from sqlalchemy import event
+# Adding security
+csrf = CSRFProtect(app)
+autocorrect_location_header = False
 
-#Only include this for SQLLite constraints
-@event.listens_for(Engine, "connect")
-def set_sqlite_pragma(dbapi_connection, connection_record):
-    # Will force sqllite contraint foreign keys
-    cursor = dbapi_connection.cursor()
-    cursor.execute("PRAGMA foreign_keys=ON")
-    cursor.close()
-"""    
 
-from app import views
-
+user_datastore = MongoEngineUserDatastore(db, User, Role)
+app.security = Security(app, user_datastore)
--- app/models.py
+++ app/models.py
@@ -1,18 +1,44 @@
 from tkinter import CASCADE
 from mongoengine import Document
-from mongoengine import DateTimeField, StringField, ReferenceField, ListField, EmailField
+from mongoengine import DateTimeField, StringField, ReferenceField, ListField, EmailField, BooleanField, IntField
+from flask_security import UserMixin, RoleMixin
 
 """
 
 Define you MongoEngine Models here
 
 """
+# Model Role
+class Role(Document, RoleMixin):
+    name = StringField(max_length=80, unique=True)
+    description = StringField(max_length=255)
+    permissions = StringField(max_length=255)
+
+# Model User
+class User(Document, UserMixin):
+    username = StringField(max_length=255, unique=True)
+    email = StringField(max_length=255, unique=True)
+    password = StringField(max_length=255)
+    active = BooleanField(default=True)
+    fs_uniquifier = StringField(max_length=64, unique=True)
+    confirmed_at = DateTimeField()
+    last_login_at = DateTimeField()
+    current_login_at = DateTimeField()
+    last_login_ipStringField = StringField (max_length=255)
+    current_login_ip = StringField (max_length=255)
+    login_count = IntField()
+    roles = ListField(ReferenceField(Role), default=[])
+
 # Model Contact
 class Contact(Document):
     nom = StringField(required=True)
     prenom = StringField(required=True)
     telephone = StringField(min_length=10, max_length=10, required=True)
     email = EmailField(required=True)
+    
+    def validate(self, clean=True):
+        return super().validate(clean)
+        
     def __unicode__(self):
         return self.nom + ' ' +self.prenom
 
